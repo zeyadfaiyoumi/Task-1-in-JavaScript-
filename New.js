52d75313b143139ad8809654f24425e558e00116
zeyadfaiyoumi <zeyadfaiyoumi@gmail.com>
2024-06-05 06:24:36 +0300

    Task-1-in-JavaScript-

--- New.js
+++ New.js
@@ -1,34 +1,26 @@
-let name = prompt("Please enter your name","zeyad")
-let gendar=prompt("please enter your gendar","must be male or female")
-if(gendar=="male"){
-
-alert("welcome Mr:"+name);
-
+let name = prompt("Please enter your name")
+let gendar=prompt("please enter your gendar")
+while(gendar !=="female"&& gendar !=="male")
+    {
+   gendar=prompt("please enter your gendar")
 }
-else if(gendar=="female"){
-    alert("welcome Ms:"+name);
+   if(gendar=="male"){
+   alert("welcome Mr: "+name);
+   }
+
+else {
+    alert("welcome Ms: "+name);
    
 }
-else{alert("welcome"+name);}
-confirm("are you want to order donut or drinks or any things other")
-let order = prompt ("please enter your order name")
-alert("there order is getting prepared")
-console.log(name+"    "+order)
 console.log(5*3)
-console.log("JavaScript"+ 50 )
-console.log(17 % 5 )
-console.log(5 % 17)
-console.log(5/10)
-console.log(4 === "4")
-console.log(4 != 5)
-console.log(7 <= 8)
-console.log("Hello" + 5)
-// console.log(Math.ceil(x) - Math.floor(x) )
-// console.log(Math.pow(x,2))
-let num1=prompt("enter num1")
-alert("num1="+num1)
-numOne=prompt("enter numOne")
-num2=prompt("enter num2")
-if(numOne>num2){
-alert(numOne+"   "+num2)}
-else{alert(num2+"   "+numOne)}
+
+
+// confirm("are you want to order donut or drinks or any things other")
+// let order = prompt ("please enter your order name")
+// alert("there order is getting prepared")
+
+
+var values=[name,gendar,order]
+for(i=0;i<values.length;i++){
+console.log(values[i]);}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Task-1-in-JavaScript-
+#I-Am-Use-Popup-Boxes-and-IFStatements-Comparison-operators-and-Logical-operators
