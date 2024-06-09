dd383c23099f95aac2b4671c1b85c87f9a4a6acd
zeyadfaiyoumi <zeyadfaiyoumi@gmail.com>
2024-06-09 18:15:42 +0300

    task one

--- New.js
+++ New.js
@@ -1,5 +1,6 @@
 let name = prompt("Please enter your name")
-// let gendar=prompt("please enter your gendar")
+let age=prompt("please enter your age")
+let order = prompt ("please enter your order name")
 function gendar(){gendar=prompt("please enter your gendar")}
 
 while(gendar !=="female"&& gendar !=="male")
@@ -14,11 +15,34 @@ else {
     alert("welcome Ms: "+name);
    
 }
+let hero1=document.getElementsByClassName("img")
+let maneTage=document.createElement("div");
+        maneTage.setAttribute("id","maneTage");
+        document.body.appendChild(maneTage);
+        
+        
+        let para=document.createElement("p");
+        maneTage.appendChild(para);
+        para.innerText=name;
+
+let orderList=document.createElement("ol");
+document.body.appendChild(orderList);
+
+for(i=0;i<3;i++){
+    let list=document.createElement("li")
+    orderList.appendChild(list)
+    if(i==0){
+        list.innerText=gendar;
+    }
+    else if(i==1){list.innerText=age;}
+
+    else{ list.innerText=order;}
+}
+
 console.log(5*3)
 
 
 confirm("are you want to order donut or drinks or any things other")
-let order = prompt ("please enter your order name")
 alert("there order is getting prepared")
 
 
